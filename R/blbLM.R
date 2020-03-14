b4986107123acb0244e56b765133b69add71fd33
Bi Nguyen <biinguyen@ucdavis>
2020-03-14 02:28:38 -0700

    finished the prediction ci

--- R/blbLM.R
+++ R/blbLM.R
@@ -1,5 +1,5 @@
 
-#' @title Bag of Little Bootstrap Confidence Interval
+#' @title Bag of Little Bootstrap Confidence Interval for Linear Regression Estimators
 #' @description blb.ceof.ci is used to calculate the bag of little bootstrap confidence interval for the linear model
 #' @param data a dataframe or tibble object
 #' @param y the name of the response variable
@@ -13,12 +13,54 @@
 #'
 #' @examples
 #' blb.coef.ci(iris, "Sepal.Length",  "Petal.Width", 10, 15, 0.05)
+#' blb.coef.ci(iris, "Sepal.Length", c("Sepal.Width", "Petal.Width"), 10, 15, 0.05)
 blb.coef.ci <- function(data, y, x, m, r, alpha){
-  ci = blb.coef.ci.list(data, y, x, m, r, alpha)
-  purrr::reduce(ci, `+`)/base::length(ci)
+  ci <- blb.coef.ci.list(data, y, x, m, r, alpha)
+  avg_ci(ci)
+}
+
+#' Bag of Little Bootstrap Confidence Interval for Sigma (Linear Model)
+#'
+#' @param data a dataframe or tibble object
+#' @param y the name of the response variable
+#' @param x the name of the indepedent variables
+#' @param m the number of group to split
+#' @param r the number of iteration for each boot
+#' @param alpha the significance level
+#'
+#' @return returns a matrix of the bootstrap ci
+#' @export
+#'
+#' @examples
+#' blb.sigma.ci(iris, "Sepal.Length",  "Petal.Width", 10, 15, 0.05)
+#' blb.sigma.ci(iris, "Sepal.Length", c("Sepal.Width", "Petal.Width"), 10, 15, 0.05)
+blb.sigma.ci <- function(data, y, x, m, r, alpha){
+  ci <- blb.sigma.ci.list(data, y, x, m, r, alpha)
+  avg_ci(ci)
 }
 
 
+#' Bag of Little Bootstrap Confidence Interval for Prediction (Linear Model)
+#'
+#' @param data a dataframe or tibble object
+#' @param pred_df a dataframe or tibble object with corresponding names for indepedent variables
+#' @param y the name of the response variable
+#' @param x the name(s) of the indepedent variables
+#' @param m the number of group to split
+#' @param r the number of iteration for each boot
+#' @param alpha the significance level
+#'
+#' @return returns a matrix of the bootstrap ci
+#' @export
+#'
+#' @examples
+#'pred_df <- tibble(Sepal.Length = c(1,2,3,4,5), Sepal.Width = c(1,2,3,4,5))
+#'blb.pred.ci(iris, df, "Petal.Width", c("Sepal.Length", "Sepal.Width"), 10, 15, 0.05)
+
+blb.pred.ci<- function(data, pred_df, y, x, m, r, alpha){
+  ci <- blb.pred.ci.list(data, pred_df, y, x, m, r, alpha)
+  avg_ci(ci)
+}
 
 
 
--- R/helper_functions.R
+++ R/helper_functions.R
@@ -2,43 +2,87 @@
 #data -> dataframe or tibble
 #m -> number of groups the user want to spot
 #return a list of dataframes
-
 #'@keywords internal
 groupSplit<- function(data, m){
-  1:m %>% purrr::map(~dplyr::sample_n(data, size = nrow(data)/m))
+  1:m %>% purrr::map(~dplyr::sample_n(data, size <- nrow(data)/m))
 }
 
 
-#multFreq is a helper function that calculates the multinomial distribution based
-#on the original size of data, n, and the size of the sub sample
-#n -> an integer that represent the original data size
-#subSample-> dataframe or tibble of the partitioned of the original data
-#returns a matrix of [1:n,1] frequency
-
+#each_boot helper function that calculates linear regression based on weights using mutlinomial distribution
+#subSample -> represent the subsample dataframe from the split
+#n -> represent the size of the original dataset
+#r -> represent the number of iteration
+#y -> represent the response variable
+#x -> represent the indepedent variables
+#returns an lm object
 #'@keywords internal
 each_boot <- function(subSample, n, r, y, x){
-  freq = stats::rmultinom(1, n, rep(1, nrow(subSample)))
-  lm(stats::formula(subSample[c(y,x)]), data = subSample, weights = freq)
+  subSample <- subSample[complete.cases(subSample), ]
+  freq <- stats::rmultinom(1, n, rep(1, base::nrow(subSample)))
+  stats::lm(stats::formula(subSample[c(y,x)]), data <- subSample, weights = freq)
+}
+
+#each_boot_pred is a helper function that calculates prediction of a linear model based
+#on weights from the mutlinomial distribution
+#subSample -> represent the subsample dataframe from the split
+#n -> represent the size of the original dataset
+#r -> represent the number of iteration
+#y -> represent the response variable
+#x -> represent the indepedent variables
+#pred_df -> represent the dataframe with the values to predict
+#returns the prediction values
+#'@keywords internal
+each_boot_pred <- function(subSample, n, r, y, x, pred_df){
+  subSample <- subSample[complete.cases(subSample), ]
+  freq <- stats::rmultinom(1, n, rep(1, base::nrow(subSample)))
+  fit <- lm(stats::formula(subSample[c(y,x)]), data = subSample, weights = freq)
+  stats::predict(fit, pred_df)
 }
 
 
-#Helper function that calculates the bootstrap percentile for sigma, coefficients, etc
+#perc.conf.int is a helper function that calculates the bootstrap percentile for sigma, coefficients, etc
+#returns list of list corresponding to the confidence interval based on alpha
 #'@keywords internal
 perc.conf.int <- function (list_obj, alpha){
-    apply(list_obj, MARGIN = 2, stats::quantile, probs = c(alpha/2, 1- alpha/2))
+    apply(list_obj, MARGIN = 2, stats::quantile, probs <- c(alpha/2, 1- alpha/2))
 }
 
-#Helper function that calculates the boostrap percentile for each group
+#blb.coef.ci.list is a helper function that calculates the boostrap percentile for the regression estimator
 #returns a list of the percentile confidence interval
 #'@keywords internal
-blb.coef.ci.list <- function(data, y, x, m, r, alpha){
-  subSamples = groupSplit(data, m)
+blb.coef.ci.list <- function(data, y, x, m, r = 10, alpha = 0.05){
+  subSamples <- groupSplit(data, m)
   subSamples %>% purrr::map(function(df){
-    #I need to do iteration
-    1:r %>% purrr::map(~each_boot(df, base::nrow(df), ., y, x))} %>%
+    1:r %>% purrr::map(~each_boot(df, base::nrow(data), ., y, x))} %>%
       purrr::map(~.$coefficient) %>%
       purrr::reduce(base::rbind) %>%
       perc.conf.int(., alpha)
     )
+}
+
+#blb.coef.ci.list is a helper function that calculates the bootstrap percentile for sigma
+#returns a list of the percentile confidence interval
+blb.sigma.ci.list <- function(data, y, x, m, r = 10, alpha = 0.05){
+  subSamples <- groupSplit(data, m)
+  subSamples %>% purrr::map(function(df){
+    1:r %>% purrr::map(~each_boot(df, nrow(data),., y,x))} %>%
+      purrr::map(base::summary) %>% purrr::map(~.$sigma)) %>%
+    purrr::map(base::unlist) %>%
+    purrr::map(stats::quantile, probs <- c(alpha/2, 1- alpha/2))
+}
+
+#blb.pred.ci.list is a helper function that calculates the bootstrap percentile for the prediction values
+#returns a list of the percentile confidence interval
+blb.pred.ci.list <- function(data, pred_df, y, x, m, r, alpha){
+  subSamples <- groupSplit(data, m)
+  subSamples %>% purrr::map(function(df){
+    1:r %>% purrr::map(~each_boot_pred(df, nrow(data), ., y, x, pred_df))
+  }) %>% purrr::map(reduce, rbind) %>%
+    purrr::map(perc.conf.int, alpha)
+}
+
+#avg_ci is a helper function that calculates the average percentile of the bootstrap methods
+avg_ci <- function(ci){
+  purrr::reduce(ci, `+`)/base::length(ci)
 }
 
